ba3b7be44c13b175bef4aafe8421cd992c63eb7b
SAWAPANDEEP <78143025+Sawapandeep@users.noreply.github.com>
2025-01-28 17:18:11 +0000

    28 Jan 22:49

--- app/containers/Header.tsx
+++ app/containers/Header.tsx
@@ -1,11 +1,28 @@
-//'use client'
+'use client'
 
+import Image from "next/image";
+import VercelLogo from "@/public/vercel.svg";
 
 const Header = () => {
 
 return (
-       <div id="header">
-        
+       <div id="headerSection" className=" w-full  top-0 absolute">
+              <div id="Landing_Page_Nav_Container" className="px-32">
+                     <div id="Landing_Page_Nav_Menu_Wrapper" className="py-4 relative z-[2]">
+                            <div id="Landing_Page_Nav_Content_Wrapper" className="flex flex-row justify-between items-center">
+                                   <div id="Logo">
+                                          <Image src={VercelLogo} className="h-8 w-auto " alt="logo"/>
+                                   </div>
+                                   <div id="Landing_Page_Nav_List_Wrapper" className="fixed w-[250px] p-6 bg-transparent ">
+                                          <div id="Landing_Page_Nav_Menu_List" className="flex flex-row">
+                                                 <li>Demo</li>
+                                                 <li>Features</li>
+                                          </div>
+                                   </div>
+                                   <div id="Landing_Page_Nav_Button" className="bg-white absolute rounded-xl text-black">Suggest</div>
+                            </div>
+                     </div>
+              </div>
        </div>
 );
 }
